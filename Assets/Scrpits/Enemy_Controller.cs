94e986a475543c723cb8d63530e681657dec72fd
Tourtles <tzvilome@gmail.com>
2023-02-05 12:30:05 -0500

    Gave the player movement to the ape

--- Assets/Scrpits/Enemy_Controller.cs
+++ Assets/Scrpits/Enemy_Controller.cs
@@ -8,23 +8,23 @@ public class Enemy_Controller : MonoBehaviour
     int cur = 0;
     public float speed = 0.3f;
 
-    void FixedUpdate ()
-    {
+    //void FixedUpdate ()
+    //{
          // Waypoint not reached yet? then move closer
-    if (transform.position != waypoints[cur].position) {
-        Vector2 p = Vector2.MoveTowards(transform.position,
-                                        waypoints[cur].position,
-                                        speed);
-        GetComponent<Rigidbody2D>().MovePosition(p);
-    }
+    //if (transform.position != waypoints[cur].position) {
+        //Vector2 p = Vector2.MoveTowards(transform.position,
+                                        //waypoints[cur].position,
+                                        //speed);
+        //GetComponent<Rigidbody2D>().MovePosition(p);
+    //}
     // Waypoint reached, select next one
-    else cur = (cur + 1) % waypoints.Length;
+    //else cur = (cur + 1) % waypoints.Length;
 
      // Animation
-    Vector2 dir = waypoints[cur].position - transform.position;
-    GetComponent<Animator>().SetFloat("DirX", dir.x);
-    GetComponent<Animator>().SetFloat("DirY", dir.y);
-    }
+    //Vector2 dir = waypoints[cur].position - transform.position;
+    //GetComponent<Animator>().SetFloat("DirX", dir.x);
+    //GetComponent<Animator>().SetFloat("DirY", dir.y);
+    //}
 
     void OnTriggerEnter2D(Collider2D co)
     {
--- Assets/Scrpits/Goblet_Script.cs
+++ Assets/Scrpits/Goblet_Script.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Goblet_Script : MonoBehaviour
 {
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +19,6 @@ public class Goblet_Script : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        Destory(gameObject);
+        Destroy(gameObject);
     }
 }
